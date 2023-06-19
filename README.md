2ef800895f43f502b573f3204a21fffbf705152e
maha-dorai <137075050+maha-dorai@users.noreply.github.com>
2023-06-19 22:01:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-alhpjk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-alhpjk)
---
+++ index.ts
@@ -0,0 +1,63 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('bonjouur');
+let num1: number;
+let num2: number;
+num1 = 123;
+num2 = 457;
+console.log(num1);
+console.log(num2);
+console.log(num1 + num2);
+//#exercise:
+let may1: number;
+let may2: number;
+let may3: number;
+may1 = 100;
+may2 = 200;
+may3 = may1 + may2;
+console.log(may3);
+//.....
+let muw0: number;
+let muw1: number;
+let muw2: number;
+let muw3: number;
+let muw4: number;
+muw0 = 2;
+muw1 = 2;
+muw2 = 2;
+muw3 = 2;
+muw4 = muw0 + muw1 + muw2 + muw3;
+console.log(muw4);
+//.....
+let pet1: number;
+let pet2: number;
+let pet3: number;
+pet1 = 400;
+pet2 = 200;
+pet3 = pet1 - pet2;
+console.log(pet3);
+//.....
+let mak1, mak2, mak3: number;
+mak1 = 200;
+mak2 = 200;
+mak3 = mak1 * mak2;
+console.log(mak3);
+//#cours:
+let sab, sab1, sab2: string;
+sab = 'bonjour mon ami';
+sab1 = ' samii';
+sab2 = sab + sab1;
+console.log(sab2);
+//#exercise:
+let mynumber: number;
+mynumber = 100 + 10;
+console.log(mynumber);
+//.....
+let mynumber1, mynumber2, mynumber3: number;
+mynumber1 = 10;
+mynumber2 = 20;
+mynumber3 = mynumber1;
+console.log('number1', (mynumber1 = mynumber2));
+console.log('number2', (mynumber2 = mynumber3));
